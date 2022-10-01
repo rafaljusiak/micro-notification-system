8e0274ac52a9b37fc73dceb60cab517221fb5802
Rafal Jusiak <jusiak.raf@gmail.com>
2022-10-01 23:45:55 +0200

    Update versions

--- app.py
+++ app.py
@@ -13,9 +13,14 @@ CORS(app, support_credentials=True)
 def send():
     client = slack.WebClient(token=os.getenv("SLACK_API_TOKEN"))
     channel = os.getenv("SLACK_NOTIFICATIONS_CHANNEL")
-    body = request.json.get("message")
-    message = f"<!channel>\n\n:fire: *Użytkownik zgłosił problem:* :fire:\n\n {body}"
-    client.chat_postMessage(channel=channel, text=message)
+
+    content = request.json.get("content")
+    email = request.json.get("email")
+    type = request.json.get("type")
+    message = request.json.get("message")
+
+    slack_message = f"<!channel>\n\n:fire: *Użytkownik zgłosił problem:* :fire:\n\n {message}"
+    client.chat_postMessage(channel=channel, text=slack_message)
 
     state = request.json.get("state", None)
     if state:
@@ -28,11 +33,37 @@ def send():
             os.getenv("JIRA_API_KEY"),
         ),
     )
-    jira.create_issue(
-        project=os.getenv("JIRA_PROJECT"),
-        summary=body.replace("\n", " ")[:80] + "...",
-        description=message,
-        issuetype={"name": "Bug"},
-    )
+
+    if content is not None:
+        email_response_example = f"""Dziękuję za zgłoszenie
+        
+    {message}
+    
+    Pozdrawiam,
+    Michał z Zespołu Moja Matura
+    """
+
+        jira.create_issue(
+            project=os.getenv("JIRA_PROJECT"),
+            summary=f"[{type}][{email}]: " + content.replace("\n", " "),
+            description=f"Użytkownik: {email}" + "\n\n" + email_response_example,
+            issuetype={"name": "Bug"},
+        )
+    else:
+        email_response_example = f"""Dziękuję za zgłoszenie
+        
+    kategoria: {type}
+    opis: {message}
+    
+    Pozdrawiam,
+    Michał z Zespołu Moja Matura
+    """
+
+        jira.create_issue(
+            project=os.getenv("JIRA_PROJECT"),
+            summary=message.replace("\n", " ")[:80] + "...",
+            description=f"Użytkownik: {email}" + "\n\n" + email_response_example,
+            issuetype={"name": "Bug"},
+        )
 
     return Response()
